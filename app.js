3a9266b5acdcb567c20e94ea9c891eb0e1979df3
aportav <andi_idfer@hotmail.com>
2019-08-08 11:12:43 -0600

    Routing parameters

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Import modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const _ = require('lodash');
 
 // Global variables
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -30,6 +31,16 @@ app.get("/contact", (req, res) => {
 
 app.get("/compose", (req, res) => {
   res.render('compose');  
+});
+
+app.get("/posts/:testing", (req, res) => {
+  // Learning about route parameters
+  if(composeArray.some(e => _.lowerCase(e.title) == _.lowerCase(req.params.testing))) {
+    console.log('Match found');
+  }
+  else {
+    console.log('not found');
+  }
 });
 
 // Post requests
